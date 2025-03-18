870a394c8fe6d38ba5d41cdeccb506b9b9da66fa
suttipkl <suttipkl@ais.co.th>
2025-03-18 23:07:24 +0700

    add build

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <DownloadComponent />
+  </div>
+</template>
+
+<script>
+import DownloadComponent from './components/DownloadComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    DownloadComponent
+  }
+};
+</script>
+
+<style>
+@import 'bootstrap/dist/css/bootstrap.css';
+html, body {
+  height: 100%;
+  margin: 0;
+}
+#app {
+  height: 100%;
+}
+</style>
