afa167f61ab015ad37157fa3435d1004cd5db477
suttipkl <suttipkl@ais.co.th>
2025-03-18 23:58:55 +0700

    fix build

--- script/build.sh
+++ script/build.sh
@@ -1,8 +1,28 @@
+rm -rf ../dist
 yarn build
+
+# Navigate to the dist directory
 cd ../dist
-git init
-git remote add origin https://github.com/suklins-ice/torpedo-document.git
+
+# Initialize git repository if not already initialized
+if [ ! -d ".git" ]; then
+  git init
+fi
+
+# Set the remote URL
+git remote set-url origin https://github.com/suklins-ice/torpedo-document.git
+
+# Pull the latest changes from the remote repository
+git pull origin gh-pages --allow-unrelated-histories
+
+# Add all files to the staging area
 git add .
+
+# Commit the changes
 git commit -m "Update document"
+
+# Set the branch to gh-pages
 git branch -M gh-pages
+
+# Push the changes to the remote repository
 git push -u origin gh-pages
